06306da7d9c7e719e49494b546ffbc2eee8e384d
B1acks1te <GitHub@bartletts.co.nz>
2026-09-19 12:55:23 +1200

    fix(ui): correct typo in label for Chance Of Upgrade

--- ui/src/components/IssuedWarningsAndWatches/index.tsx
+++ ui/src/components/IssuedWarningsAndWatches/index.tsx
@@ -279,7 +279,7 @@ function AlertCard({ issuedAlert }: { issuedAlert: IssuedAlert }) {
             <span>{getPeriodDescription(onset, expires)}</span>
           </div>
           <div>
-            <span className="font-bold">Chance Of sUpgrade: </span>
+            <span className="font-bold">Chance Of Upgrade: </span>
             <span>{ChanceOfUpgrade || 'N/A'}</span>
           </div>
 
